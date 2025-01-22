bed34d7b1586107dd0c8e92e2e346d959410899c
naveen-ecgroup <naveen.n@ecgroup-intl.com>
2025-01-22 17:24:37 +0530

    Added Cucumber stack reports

--- src/test/java/runner/RunnerClass.java
+++ src/test/java/runner/RunnerClass.java
@@ -1,15 +1,20 @@
 package runner;
 
+
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+
+
 @CucumberOptions(
-		features = "src/test/resources/features/", 		// Path to feature files
+		features = "src/test/resources/features/homepage.feature", 		// Path to feature files
 		glue = "utilities", 											// Package containing step definitions
 		plugin = { 
 					"pretty", 											// Prints detailed logs in the console
 					"html:target/cucumber-reports.html", 				// Generates an HTML report
-					"json:target/cucumber-reports.json" 				// Generates a JSON report
+					"json:target/cucumber-reports.json",				// Generates a JSON report
+					"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 		},
 		monochrome = true 												// Makes console output more readable
 )
