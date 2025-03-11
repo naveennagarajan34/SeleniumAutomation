38e99f365ed9c85a56fb96da8365754f3bd53b27
Naveen N <78733685+naveennagarajan34@users.noreply.github.com>
2025-03-11 20:32:53 +0530

    Code added for Users page copy URL

--- src/test/java/pages/UsersPage.java
+++ src/test/java/pages/UsersPage.java
@@ -26,7 +26,13 @@ public class UsersPage {
 	private WebElement sendButton;
 	@FindBy(css="div.mat-mdc-snack-bar-label[matsnackbarlabel]")
 	private WebElement snackbar;
-	
+	@FindBy(xpath="//span[text()=' Share ']/parent::button")
+	private WebElement shareBtn;
+	@FindBy(css="a[title='Change invitation language']")
+	private WebElement uiChange;
+	@FindBy(css="div.mat-mdc-dialog-actions button.mat-unthemed")
+	private WebElement copyButton;
+
 	
 	public UsersPage(WebDriver driver) {
 		this.driver = driver;
@@ -34,7 +40,7 @@ public class UsersPage {
 	}
 	
 	public void clickOnUsersFromNavBar() throws InterruptedException {
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		WebElement usersLink = wait.until(ExpectedConditions.presenceOfElementLocated(
 			    By.xpath("//mat-drawer[@id='menu-drawer']//a[contains(@href, '/users')]")
@@ -53,7 +59,7 @@ public class UsersPage {
 		WebElement roleSelect = driver.findElement(By.xpath("//mat-option//div[contains(text(),'"+role+"')]"));
 		roleSelect.click();
 		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
 		
 		localeBox.click();
 		WebElement localeSelect = driver.findElement(By.xpath("//mat-option//span[contains(text(),'"+locale+"')]"));
@@ -67,4 +73,30 @@ public class UsersPage {
 	    
 	    assertTrue(snackbar.getText().contains("email has been sent"),"Snackbar text does not contain expected text");
 	}
+	
+	public void clickShareButton() {
+
+		String inviteUi = "Bahasa";		// need to be updated for dynamic selection
+		shareBtn.click();
+		uiChange.click();
+		
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Wait up to 10 seconds
+	    wait.until(ExpectedConditions.visibilityOf(uiChange));
+		
+		WebElement inviteLanguage = driver.findElement(By.xpath("//mat-list-option/span/child::span[contains(text(),'"+inviteUi+"')]/ancestor::mat-list-option"));
+		
+	    wait.until(ExpectedConditions.visibilityOf(inviteLanguage));
+		
+		inviteLanguage.click();
+		wait.until(ExpectedConditions.visibilityOf(copyButton));
+		copyButton.click();
+	}
+	
+	public void verifyLinkCopied() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Wait up to 10 seconds
+	    wait.until(ExpectedConditions.visibilityOf(snackbar));
+	    
+	    assertTrue(snackbar.getText().contains("copied"),"Snackbar text does not contain expected text");
+	}
+	
 }
