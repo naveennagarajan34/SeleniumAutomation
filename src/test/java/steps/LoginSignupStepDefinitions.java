29ece148923d406c630c4576c4196ba879f14812
Naveen N <78733685+naveennagarajan34@users.noreply.github.com>
2025-08-06 19:04:14 +0530

    TC tags added for login feature

--- src/test/java/steps/LoginSignupStepDefinitions.java
+++ src/test/java/steps/LoginSignupStepDefinitions.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.HeaderClass;
 import pages.HomePage;
 import pages.LoginSignup;
 import utilities.BrowserClass;
@@ -46,15 +48,21 @@ public class LoginSignupStepDefinitions {
 
 		ReadWriteExcel.loadExcelFile(readPropObject.getProperty("excel_data_path"), "credentials");
 
-		String username = ReadWriteExcel.getCellValue(1, 0);
-		String password = ReadWriteExcel.getCellValue(1, 1);
-		String loginType = ReadWriteExcel.getCellValue(1, 2);
+		String username = ReadWriteExcel.getCellValue(2, 0);
+		String password = ReadWriteExcel.getCellValue(2, 1);
+		String loginType = ReadWriteExcel.getCellValue(2, 2);
 		loginObject = new LoginSignup(driver);
 		if (loginType.equalsIgnoreCase("email")) {
 			driver = loginObject.loginWithCredentials(username, password);
 		} else if (loginType.equalsIgnoreCase("paratext")) {
 			driver = loginObject.loginWithParatext(username, password);
 		}
+	}
+	
+	@And("verify that the user is loggedin successfully")
+	public void verify_user_loggedin() {
+		HeaderClass header= new HeaderClass(driver);
+		header.verifyHeaderIsVisible();
 	}
 	
 	@Then("click on the logout button and verify")
@@ -64,4 +72,17 @@ public class LoginSignupStepDefinitions {
 		loginObject.logoutBtnclick();
 		
 	}
+	
+	@Then("login with invalid {string} and {string}")
+	public void login_with_invalid_credentials(String username, String password) {
+		loginObject = new LoginSignup(driver);
+		loginObject.login_with_invalid_credentials(username, password);
+	}
+	
+	@And("verify that wrong email or password banner appeared")
+	public void verify_invalid_credentials_login () {
+		loginObject = new LoginSignup(driver);
+		loginObject.wrongEmailOrPasswordMessage();
+	}
+	
 }
